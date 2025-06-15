59cbcf9346f099b04cca5e68d92728b223d918f0
Jack Hillz <mysticteck@gmail.com>
2025-06-14 14:18:58 -1200

    Fix token loading for Railway

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from discord.ext import commands
 import logging
 import os
 
-if os.getenv("RENDER") is None:  # Not running on Render
+import os
+
+# Only use load_dotenv locally
+if os.path.exists(".env"):
     from dotenv import load_dotenv
     load_dotenv()
 
